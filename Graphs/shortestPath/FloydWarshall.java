81e81d092936e6b2b37b2d07e06c140312ebe3ec
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2025-04-16 18:52:08 +0530

    Create FloydWarshall.java

---
+++ Graphs/shortestPath/FloydWarshall.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void floydWarshall(int[][] dist) {
+        int n = dist.length;
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (dist[i][k] != 100000000 && dist[k][j] != 100000000) {
+                        if ((long) dist[i][k] + dist[k][j] < dist[i][j]) {
+                            dist[i][j] = dist[i][k] + dist[k][j];
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
